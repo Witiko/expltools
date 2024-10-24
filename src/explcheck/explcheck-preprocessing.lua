9be19fff430d1686756b6809efffa0d6afc73ba4
Vít Starý Novotný <witiko@mail.muni.cz>
2024-10-24 16:26:40 +0200

    Support commands `\ExplSyntax*`

--- src/explcheck/explcheck-preprocessing.lua
+++ src/explcheck/explcheck-preprocessing.lua
@@ -1,13 +1,57 @@
 local lpeg = require("lpeg")
+local P, S = lpeg.P, lpeg.S
 
 -- Define base parsers.
-local expl_syntax_on = lpeg.P([[\ExplSyntaxOn]])
-local expl_syntax_off = lpeg.P([[\ExplSyntaxOff]])
-local any = lpeg.P(1)
+---- Generic
+local any = P(1)
 local eof = -any
 
+---- Tokens
+local lbrace = P("{")
+local rbrace = P("}")
+local percent_sign = P("%")
+
+---- Spacing
+local newline = P("\n")
+              + P("\r\n")
+              + P("\r")
+local linechar = any - newline
+local spacechar = S("\t ")
+
+-- Define intermediate parsers.
+---- Parts of TeX syntax
+local comment = percent_sign
+              * linechar^0
+              * newline
+              * spacechar^0
+local argument = lbrace
+               * (
+                 comment
+                 + (any - rbrace)
+               )^0
+               * rbrace
+
+---- Standard delimiters
+local provides = P([[\ProvidesExpl]])
+               * (
+                   P("Package")
+                   + P("Class")
+                   + P("File")
+                 )
+               * spacechar^0
+               * argument
+               * comment^-1
+               * argument
+               * comment^-1
+               * argument
+               * comment^-1
+               * argument
+local expl_syntax_on = P([[\ExplSyntaxOn]])
+local expl_syntax_off = P([[\ExplSyntaxOff]])
+
 -- Define top-level grammar rules.
-local Opener = expl_syntax_on  -- TODO: Add other openers: `\ProvidesExplPackage`, `\ProvidesExplClass`, and `\ProvidesExplFile`.
+local Opener = expl_syntax_on
+             + provides
 local Closer = expl_syntax_off
 
 local function preprocessing(state)
