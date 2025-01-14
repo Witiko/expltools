ce864eb378925235da942daca3525abdaa75158a
Vít Starý Novotný <witiko@mail.muni.cz>
2025-01-14 19:50:57 +0100

    Do not produce S204 for an empty grouping (`{}`)

--- explcheck/src/explcheck-lexical-analysis.lua
+++ explcheck/src/explcheck-lexical-analysis.lua
@@ -83,15 +83,17 @@ local function lexical_analysis(issues, all_content, expl_ranges, options)  -- l
         local character, catcode, character_index_increment = get_character_and_catcode(character_index)
         local range_start = map_back(character_index)
         local range_end = range_start + 1
-        if (  -- a potential missing stylistic whitespace
-              previous_catcode == 0  -- right after a control sequence
-              or previous_catcode == 1 or previous_catcode == 2  -- or a begin/end grouping
+        if (
+              catcode ~= 9 and catcode ~= 10  -- a potential missing stylistic whitespace
+              and (
+                previous_catcode == 0  -- right after a control sequence
+                or previous_catcode == 1 or previous_catcode == 2  -- or a begin/end grouping
+              )
             ) then
           if (
-                catcode ~= 0 and catcode ~= 1  -- for a control sequence of being grouping, we will handle the lack of whitespace elsewhere
-                and not (previous_catcode == 2 and character == ",")  -- allow a comma after end grouping without a whitespace in between
-                and not (previous_catcode == 1 and catcode == 6)  -- allow a parameter after begin grouping  without a whitespace in between
-                and catcode ~= 9 and catcode ~= 10
+                catcode ~= 0 and catcode ~= 1 and catcode ~= 2  -- for a control sequence or begin/end grouping, we handle this elsewhere
+                and not (previous_catcode == 2 and character == ",")  -- allow a comma after end grouping without whitespace in between
+                and not (previous_catcode == 1 and catcode == 6)  -- allow a parameter after begin grouping without whitespace in between
               ) then
             issues:add('s204', 'missing stylistic whitespaces', range_start, range_end)
           end
@@ -126,7 +128,7 @@ local function lexical_analysis(issues, all_content, expl_ranges, options)  -- l
           local csname = table.concat(csname_table)
           range_end = map_back(previous_csname_index) + 1
           table.insert(tokens, {"control sequence", csname, 0, range_start, range_end})
-          if previous_catcode ~= 9 and previous_catcode ~= 10 then
+          if previous_catcode ~= 9 and previous_catcode ~= 10 then  -- a missing stylistic whitespace
             issues:add('s204', 'missing stylistic whitespaces', range_start, range_end)
           end
           previous_catcode = 0
@@ -163,7 +165,11 @@ local function lexical_analysis(issues, all_content, expl_ranges, options)  -- l
                 issues:add('e208', 'too many closing braces', range_start, range_end)
               end
             end
-            if previous_catcode ~= 9 and previous_catcode ~= 10 and not (previous_catcode == 6 and catcode == 2) then
+            if (
+                    previous_catcode ~= 9 and previous_catcode ~= 10  -- a potential missing stylistic whitespace
+                    and not (previous_catcode == 1 and catcode == 2)  -- allow an end grouping immediately after begin grouping
+                    and not (previous_catcode == 6 and catcode == 2)  -- allow a parameter before end grouping without whitespace in between
+                ) then
               issues:add('s204', 'missing stylistic whitespaces', range_start, range_end)
             end
             previous_catcode = catcode
