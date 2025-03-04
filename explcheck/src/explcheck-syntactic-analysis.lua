dcafbd422bca73f5ecf6447028cac959ca1261d2
Vít Starý Novotný <witiko@mail.muni.cz>
2025-03-04 16:29:32 +0100

    Make lexical analysis produce a table of groupings for each expl3 part

--- explcheck/src/explcheck-syntactic-analysis.lua
+++ explcheck/src/explcheck-syntactic-analysis.lua
@@ -6,7 +6,8 @@ local lpeg = require("lpeg")  -- luacheck: ignore lpeg
 
 -- Convert the content to a tree of function calls an register any issues.
 local function syntactic_analysis(pathname, all_content, issues, results, options)  -- luacheck: ignore pathname all_content issues options
-  local tokens = results.tokens  -- luacheck: ignore tokens
+  local all_tokens = results.tokens  -- luacheck: ignore all_tokens
+  local all_groupings = results.groupings  -- luacheck: ignore all_groupings
 end
 
 return syntactic_analysis
