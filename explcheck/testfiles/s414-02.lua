4e3cec9dff977fa715d67f21471e1076fd5ed0de
Vít Starý Novotný <witiko@mail.muni.cz>
2025-08-13 19:52:38 +0200

    Fix style issues

--- explcheck/testfiles/s414-02.lua
+++ explcheck/testfiles/s414-02.lua
@@ -1,8 +1,8 @@
 local new_issues = require("explcheck-issues")
 local preprocessing = require("explcheck-preprocessing")
 local lexical_analysis = require("explcheck-lexical-analysis")
-local semantic_analysis = require("explcheck-semantic-analysis")
 local syntactic_analysis = require("explcheck-syntactic-analysis")
+local semantic_analysis = require("explcheck-semantic-analysis")
 
 local filename = "s414-02.tex"
 
@@ -15,6 +15,8 @@ local results = {}
 
 preprocessing.process(filename, content, issues, results, options)
 lexical_analysis.process(filename, content, issues, results, options)
+syntactic_analysis.process(filename, content, issues, results, options)
+semantic_analysis.process(filename, content, issues, results, options)
 
 assert(#issues.errors == 0)
 assert(#issues.warnings == 0)
