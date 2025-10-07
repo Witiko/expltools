da34f046d93ede57f759a81705174b09406ba162
Vít Starý Novotný <witiko@mail.muni.cz>
2025-10-07 14:03:27 +0200

    Fix assertion error

--- explcheck/src/explcheck-flow-analysis.lua
+++ explcheck/src/explcheck-flow-analysis.lua
@@ -7,14 +7,16 @@ local get_option = require("explcheck-config").get_option
 local function is_confused(pathname, results, options)
   local format_percentage = require("explcheck-format").format_percentage
   local evaluation = require("explcheck-evaluation")
-  local count_tokens, count_well_understood_tokens = evaluation.count_tokens, evaluation.count_well_understood_tokens
-  local num_tokens, num_well_understood_tokens = count_tokens(results), count_well_understood_tokens(results)
+  local count_tokens = evaluation.count_tokens
+  local num_tokens = count_tokens(results)
   assert(num_tokens ~= nil)
-  assert(num_well_understood_tokens ~= nil)
   if num_tokens == 0 then
     return false
   end
   assert(num_tokens > 0)
+  local count_well_understood_tokens = evaluation.count_well_understood_tokens
+  local num_well_understood_tokens = count_well_understood_tokens(results)
+  assert(num_well_understood_tokens ~= nil)
   local min_code_coverage = get_option('min_code_coverage', options, pathname)
   local code_coverage = num_well_understood_tokens / num_tokens
   if code_coverage < min_code_coverage then
