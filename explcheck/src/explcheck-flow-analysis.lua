91392bda70e3fcdaad79fa3bfd541f4648ee0493
Vít Starý Novotný <witiko@mail.muni.cz>
2026-01-16 11:22:59 +0100

    Process just file `enumext.sty`

--- explcheck/src/explcheck-flow-analysis.lua
+++ explcheck/src/explcheck-flow-analysis.lua
@@ -321,7 +321,7 @@ local function draw_dynamic_edges(states, file_number, options)  -- luacheck: ig
   local should_continue = false
   for _, state in ipairs(states) do
     local basename = get_basename(state.pathname)
-    if basename == "chemmacros-2015-02-08.sty" then
+    if basename == "enumext.sty" then
       should_continue = true
     end
     table.insert(pathname_group, state.pathname)
