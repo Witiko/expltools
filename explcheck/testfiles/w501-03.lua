fa2be97ff53aa7703eb188a4fe3fa16e2397f70e
Vít Starý Novotný <witiko@mail.muni.cz>
2026-03-24 11:32:21 +0100

    Add `w501-01.lua` and `w501-03.lua`

---
+++ explcheck/testfiles/w501-03.lua
@@ -0,0 +1,24 @@
+local sort_issues = require("explcheck-issues").sort_issues
+local utils = require("explcheck-utils")
+
+local filename = "w501-03.tex"
+local options = {
+  expl3_detection_strategy = "always",
+  stop_after = "flow analysis",
+}
+local state = table.unpack(utils.process_files({filename}, options))
+local issues, results = state.issues, state.results
+
+assert(#issues.errors == 0)
+assert(#issues.warnings == 1)
+
+local expected_line_numbers = {{9, 12}}
+for index, warning in ipairs(sort_issues(issues.warnings)) do
+  assert(warning[1] == "w501")
+  assert(warning[2] == "multiply defined function variant")
+  local byte_range = warning[3]
+  local start_line_number = utils.convert_byte_to_line_and_column(results.line_starting_byte_numbers, byte_range:start())
+  local end_line_number = utils.convert_byte_to_line_and_column(results.line_starting_byte_numbers, byte_range:stop())
+  assert(start_line_number == expected_line_numbers[index][1])
+  assert(end_line_number == expected_line_numbers[index][2])
+end
